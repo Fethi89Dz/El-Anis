37e7d1d0b3a19cd1f34930f67a904739679295e7
El Orabi Mohamed Ikbal <ikbalix@gmail.com>
2018-08-02 15:57:55 +0100

    First commit

---
+++ src/pages/onboarding/onboarding.ts
@@ -0,0 +1,67 @@
+import { Component, ViewChild, trigger, transition, style, state, animate, keyframes } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides  } from 'ionic-angular';
+
+/**
+ * Generated class for the OnboardingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-onboarding',
+  templateUrl: 'onboarding.html',
+  animations: [
+
+    trigger('bounce', [
+      state('*', style({
+        transform: 'translateX(0)'
+      })),
+      transition('* => rightSwipe', animate('700ms ease-out', keyframes([
+        style({transform: 'translateX(0)', offset: 0}),
+        style({transform: 'translateX(-65px)', offset: .3}),
+        style({transform: 'translateX(0)', offset: 1})
+      ]))),
+      transition('* => leftSwipe', animate('700ms ease-out', keyframes([
+        style({transform: 'translateX(0)', offset: 0}),
+        style({transform: 'translateX(65px)', offset: .3}),
+        style({transform: 'translateX(0)', offset: 1})
+      ])))
+    ])
+  ]
+})
+export class OnboardingPage {
+
+  @ViewChild(Slides) slides: Slides;
+  skipMsg: string = "Skip";
+  state: string = 'x';
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  skip() {
+    this.navCtrl.push('LoginPage');
+  }
+
+  slideChanged() {
+    if (this.slides.isEnd())
+      this.skipMsg = "Alright, I got it";
+  }
+
+  slideMoved() {
+    if (this.slides.getActiveIndex() >= this.slides.getPreviousIndex())
+      this.state = 'rightSwipe';
+    else
+      this.state = 'leftSwipe';
+  }
+
+  animationDone() {
+    this.state = 'x';
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad OnboardingPage');
+  }
+
+}
